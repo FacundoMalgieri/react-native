94c359c68daa57f52a490405b526af9043c4c6de
FacundoMalgieri <facundo.malgieri@gmail.com>
2018-03-23 17:24:18 -0300

    Working

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -27,6 +27,11 @@ class FindPlaceScreen extends Component {
     }
 
     onNavigatorEvent = event => {
+        if (event.type === "ScreenChangedEvent") {
+            if (event.id === "willAppear") {
+                this.props.onLoadPlaces();
+            }
+        }
         if (event.type === 'NavBarButtonPress') {
             if (event.id === 'sideDrawerToggle') {
                 this.props.navigator.toggleDrawer({side: 'left'});
@@ -131,6 +136,5 @@ const mapDispatchToProps = dispatch => {
         onLoadPlaces: () => dispatch(getPlaces())
     }
 };
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(FindPlaceScreen);
